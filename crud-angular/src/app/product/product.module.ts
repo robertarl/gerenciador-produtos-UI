2e5c049499b725277cf954c94bd63a0ac8c4b16c
Roberta Lima <rbnow12@gmail.com>
2022-07-02 16:57:20 -0300

    Componentes de atualização de produtos

--- crud-angular/src/app/product/product.module.ts
+++ crud-angular/src/app/product/product.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { ProductRoutingModule } from './product-routing.module';
 import { ProductsComponent } from './products/products.component';
 import { ProductCreateComponent } from './product-create/product-create.component';
+import { ProductUpdateComponent } from './product-update/product-update.component';
 
 
 @NgModule({
   declarations: [
     ProductsComponent,
-    ProductCreateComponent
+    ProductCreateComponent,
+    ProductUpdateComponent
   ],
   imports: [
     CommonModule,
