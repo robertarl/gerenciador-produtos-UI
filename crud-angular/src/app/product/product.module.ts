6b938dc7f44977a54cd9aed2b9408e51c1b513ae
Roberta Lima <rbnow12@gmail.com>
2022-07-02 16:55:29 -0300

    Componente de criação de formulário

--- crud-angular/src/app/product/product.module.ts
+++ crud-angular/src/app/product/product.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { ProductRoutingModule } from './product-routing.module';
 import { ProductsComponent } from './products/products.component';
+import { ProductCreateComponent } from './product-create/product-create.component';
 
 
 @NgModule({
   declarations: [
-    ProductsComponent
+    ProductsComponent,
+    ProductCreateComponent
   ],
   imports: [
     CommonModule,
