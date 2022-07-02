6b4ddabf0c25d62bac4ba91e7b982eed62314971
Roberta Lima <rbnow12@gmail.com>
2022-07-02 16:50:07 -0300

    Componentes produtos criados

--- crud-angular/src/app/product/product.module.ts
+++ crud-angular/src/app/product/product.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProductRoutingModule } from './product-routing.module';
+import { ProductsComponent } from './products/products.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProductsComponent
+  ],
   imports: [
     CommonModule,
     ProductRoutingModule
