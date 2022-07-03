9fd1a8e7169591d8b936fcb95d50e9eca3e09d01
Roberta Lima <rbnow12@gmail.com>
2022-07-03 00:02:46 -0300

    Formulario para cadastro dos produtos

--- crud-angular/src/app/product/product-create/product-create.component.ts
+++ crud-angular/src/app/product/product-create/product-create.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ProductService } from '../services/product.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-product-create',
@@ -7,9 +11,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCreateComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup
 
-  ngOnInit(): void {
+  constructor(private formBilder: FormBuilder,
+              private service: ProductService,
+              private snackBar: MatSnackBar,
+              private location: Location) {
+    this.form = this.formBilder.group({
+      name: [null],
+      price: 0
+    });
   }
 
+  ngOnInit(): void {
+
+  }
+
+  createProduct(){
+    this.service.save(this.form.value)
+                .subscribe(data => this.sucess(),
+                error =>this.onError());
+  }
+
+  cancel(){
+    this.location.back();
+  }
+
+  private sucess(){
+    this.snackBar.open('Produto salvo com sucesso!', 'Close', {duration: 3000});
+    this.cancel();
+  }
+
+  private onError(){
+    this.snackBar.open('Erro ao salvar produto!', 'Close', {duration: 3000});
+  }
 }
